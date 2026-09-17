a896294879908d7310eb7e3f5d2d49a6fa53b578
Hermansyah <abangherman2345@gmail.com>
2026-09-17 15:30:04 +0700

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,13 @@
+-- ========================================================
+-- Learn SQL From Scratch Series - Kelas23
+-- Episode 1: Setup Workspace & CREATE DATABASE
+-- File: init.sql
+-- ========================================================
+
+-- 1. Create a New Database
+-- Sets up a dedicated storage space for the Kelas23 project
+CREATE DATABASE kelas23_db;
+
+-- 2. Activate the Database
+-- Switches to the newly created database so future commands run inside it
+USE kelas23_db;
